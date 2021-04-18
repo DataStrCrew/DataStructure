394852ae1de793835e1381592de391f0fc7677fa
Mustafa Gurler <mustafagurler342@gmail.com>
2021-04-18 22:36:07 +0300

    Create PremiumReader.java

---
+++ src/PremiumReader.java
@@ -0,0 +1,9 @@
+/**
+ * Librarian of the library
+ * @author Mustafa Gurler
+ */
+public class PremiumReader extends StandartReader{
+    public PremiumReader() throws Exception {
+        super();
+    }
+}

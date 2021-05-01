0c15e0c510079a7121c377f0bce2434bc9639fc3
yesimyalc <y.yalcin072@gmail.com>
2021-05-01 19:52:54 +0300

    Task Class is created
    Task class holds the task's name and it's difficulty level. It is a comparable class.

--- src/Task.java
+++ src/Task.java
@@ -1,51 +1,34 @@
-public class Task {
 
-	private int taskDifficulty;
-	private int TaskId;
-	private boolean doneTask;
+public class Task implements Comparable
+{
+	String taskName;
+	Integer difficultyLevel;
 	
-    /**
-     * Getter for taskDifficult
-     * @return 
-     */
-	public int getTaskDifficulty() {
-		return taskDifficulty;
-	}
-    /**
-     * Setter for taskDifficult
-     * @param taskDifficulty Task difficulty of the task
-     */
-	public void setTaskDifficulty(int taskDifficulty) {
-		this.taskDifficulty = taskDifficulty;
-	}
-    /**
-     * Getter for TaskId
-     * @return
-     */
-	public int getTaskId() {
-		return TaskId;
-	}
-    /**
-     * Setter for TaskId
-     * @param taskId Task Id of the task
-     */
-	public void setTaskId(int taskId) {
-		TaskId = taskId;
-	}
-    /**
-     * Getter for doneTask
-     * @return
-     */
-	public boolean isDoneTask() {
-		return doneTask;
-	}
-    /**
-     * Setter for doneTask
-     * @param doneTask Variable that keeps the task done or not
-     */
-	public void setDoneTask(boolean doneTask) {
-		this.doneTask = doneTask;
+	public Task(String name, Integer level)
+	{
+		taskName=name;
+		difficultyLevel=level;
 	}
 	
+	/**Returns the taskName*/
+	public String getTask() {return taskName;}
+	
+	/**Returns the task's difficulty level*/
+	public Integer getLevel() {return difficultyLevel;}
+	
+	/**
+	 * Firstly compares the difficulty levels of the tasks. 
+	 * If the difficulty levels are the same, then compares the taskNames in alphabetical order.
+	 * @return 0 if they are same, a value smaller than 0 if the current task is easier, a value bigger than 0 if the current task is harderç
+	 */
+	public int compareTo(Object o) 
+	{
+		int levelComparison=difficultyLevel.compareTo(((Task) o).getLevel());
+		
+		if(levelComparison==0)
+			levelComparison=taskName.compareTo(((Task) o).getTask());
+		
+		return levelComparison;
+	}
 	
 }
